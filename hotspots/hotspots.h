8adf2974d1394b85a3c016d938e5c1f87b559b5a
Richard Wen <rwen1@umd.edu>
2024-03-23 15:10:51 -0400

    initial commit

---
+++ hotspots/hotspots.h
@@ -0,0 +1,21 @@
+#include <atomic>
+#include "parlay/primitives.h"
+#include "parlay/sequence.h"
+
+template <typename vertex, typename graph>
+auto hotspots(parlay::sequence<vertex> U, const graph& G) {
+	vertex n = G.size();
+
+	// n indicates that the vertex has not been visited yet
+	auto nearest = parlay::tabulate<std::atomic<vertex>>(n, [&] (long i) {return n;});
+
+	// mark the set U as visited from self
+	parlay::parallel_for(0, U.size(), [&] (long i) {
+			nearest[U[i]] = U[i];});
+
+	// to be filled in
+	std::cout << "unimplemented" << std::endl;
+
+	// convert from sequence of atomics to a regular sequence
+	return parlay::tabulate(n, [&] (long i) {return nearest[i].load();});
+}
