c507eec3b01f3d1edcc14d2618813d2133b2fd9e
Arjun <javathunderman@outlook.com>
2024-05-09 23:33:22 -0400

    Implement adapted par BFS for hotspots

--- hotspots/hotspots.h
+++ hotspots/hotspots.h
@@ -14,10 +14,26 @@ auto hotspots(parlay::sequence<vertex> U, const graph& G) {
 
 	// mark the set U as visited from self
 	parlay::parallel_for(0, U.size(), [&] (long i) {
-			nearest[U[i]] = U[i];});
+		nearest[U[i]] = U[i];
+	});
 
-	// to be filled in
-	std::cout << "unimplemented" << std::endl;
+	// Code taken from Parlay's parallel BFS implementation, with modifications
+	parlay::parallel_for(0, U.size(), [&] (long i) {
+		parlay::sequence<vertex> frontier = G[U[i]];
+		parlay::sequence<parlay::sequence<vertex>> frontiers;
+		while (frontier.size() > 0) {
+			frontiers.push_back(frontier);
+
+			// get out edges of the frontier and flatten
+			auto out = flatten(map(frontier, [&] (vertex u) {return G[u];}));
+
+			// keep the v that succeed in setting the visited array
+			frontier = filter(out, [&] (auto&& v) {
+				// expand the frontier by checking which don't have a hotspot yet, and use CAS to set it
+				return (nearest[v] == n) && nearest[v].compare_exchange_strong(n, U[i]);
+			});
+		}
+	});
 
 	// convert from sequence of atomics to a regular sequence
 	return parlay::tabulate(n, [&] (long i) {return nearest[i].load();});
